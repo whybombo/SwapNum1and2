f9efe731687d7f7e6cd00c3dc6b7f40793202406
Blake Nelson <168068011+whybombo@users.noreply.github.com>
2024-08-22 03:47:19 -0500

    Add files via upload

---
+++ swap numbers temp.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter the number for number 1: "))
+num2 = int(input("enter the number for number 2: "))
+
+print("Before swapping number 1 = {} and number 2 = {}".format(num1,num2))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping number1 = {} and number2 = {}".format(num1,num2))
